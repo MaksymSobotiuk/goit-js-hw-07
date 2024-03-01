e77fc9d8389ad2fca4647dc6e4aa193903973761
MaksymSobotiuk <maxim.4.july.2001@gmail.com>
2024-03-01 12:51:58 +0200

    4-6 done

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,19 @@
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.querySelector('.change-color').addEventListener('click', function () {
+  const body = document.body;
+  const colorSpan = document.querySelector('.color');
+
+  const randomColor = getRandomHexColor();
+
+  body.style.backgroundColor = randomColor;
+  
+  colorSpan.textContent = randomColor;
+
+  colorSpan.style.color = randomColor;
+});
